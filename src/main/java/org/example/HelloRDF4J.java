7829815385ae3d835058e8c3b656e6e7157bf451
Harri Valkonen <valkoneh@gmail.com>
2020-09-03 08:00:10 +0300

    Read ontology to db

--- src/main/java/org/example/HelloRDF4J.java
+++ src/main/java/org/example/HelloRDF4J.java
@@ -1,6 +1,11 @@
 package org.example;
+//package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
@@ -15,14 +20,16 @@ import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryResult;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParseException;
 import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.eclipse.rdf4j.sail.nativerdf.NativeStore;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.eclipse.rdf4j.sail.inferencer.fc.SchemaCachingRDFSInferencer;
-import org.eclipse.rdf4j.sail.memory.MemoryStore; //for testing
+
 
 public class HelloRDF4J {
 
@@ -47,13 +54,14 @@ public class HelloRDF4J {
 			model.setNamespace(FOAF.NS); 
 			model.setNamespace("ex", namespace);
 			Rio.write(model, System.out, RDFFormat.TURTLE);
+			rep.shutDown();
 		}
 		
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 
 		OWLOntology ontology = null;
 		try {
-			ontology = manager.loadOntologyFromOntologyDocument(new File("C:/Users/Default User.Lenovo/Documents/yamk/YAMK/THESIS/Onto-CMDB.owl"));
+			ontology = manager.loadOntologyFromOntologyDocument(new File("C:/Users/Default User.Lenovo/Documents/yamk/YAMK/THESIS/Onto-CMDB.ttl"));
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -62,7 +70,52 @@ public class HelloRDF4J {
 		System.out.println("Ontology : " + ontology.getOntologyID());
 		System.out.println("Format      : " + manager.getOntologyFormat(ontology)); 
 		
+		String filename = "C:/Users/Default User.Lenovo/Documents/yamk/YAMK/THESIS/Onto-CMDB.ttl";
+		
+		InputStream input = null;
+		try {
+			input = new FileInputStream(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Model model = null;
+		try {
+			model = Rio.parse(input, "", RDFFormat.TURTLE);
+		} catch (RDFParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedRDFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Repository db = new SailRepository(new MemoryStore());
+		db.init();
+		
+		try (RepositoryConnection conn = db.getConnection()) {
+			// add the model
+			conn.add(model);
+
+			// let's check that our data is actually in the database
+			try (RepositoryResult<Statement> result = conn.getStatements(null, null, null);) {
+				while (result.hasNext()) {
+					Statement st = result.next();
+					System.out.println("db contains: " + st);
+				}
+			}
+		}
+		finally {
+			// before our program exits, make sure the database is properly shut down.
+			db.shutDown();
+		}
+		
 		}
 	
+		
 
 }
